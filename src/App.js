f076db59b08baf650cfd41a90da3e23db981fbd0
Jeffrey Lin <afunpub@gmail.com>
2021-06-12 00:27:21 +0800

    01-getting-start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Todo from "./components/Todo";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>My 代辦事項</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
+    </div>
+  );
 }
 
 export default App;
